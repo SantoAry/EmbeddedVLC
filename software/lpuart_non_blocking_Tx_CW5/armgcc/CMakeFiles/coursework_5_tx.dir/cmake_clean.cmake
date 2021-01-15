250060d4ff3f70c4a64feb9cfcda754ecb63e2a8
SantoAry <72742988+SantoAry@users.noreply.github.com>
2021-01-15 07:32:11 +0000

    Location of the source codes
    The main source codes are:
    - coursework_tx_main.c
    - coursework_rx_main.c

---
+++ software/lpuart_non_blocking_Tx_CW5/armgcc/CMakeFiles/coursework_5_tx.dir/cmake_clean.cmake
@@ -0,0 +1,24 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/coursework_5_tx.dir/C_/Freescale/KSDK_1.3.0/TEST/platform/devices/MKL03Z4/startup/gcc/startup_MKL03Z4.S.obj"
+  "CMakeFiles/coursework_5_tx.dir/C_/Freescale/KSDK_1.3.0/TEST/platform/devices/MKL03Z4/startup/system_MKL03Z4.c.obj"
+  "CMakeFiles/coursework_5_tx.dir/C_/Freescale/KSDK_1.3.0/TEST/platform/devices/startup.c.obj"
+  "CMakeFiles/coursework_5_tx.dir/C_/Freescale/KSDK_1.3.0/TEST/platform/utilities/src/fsl_debug_console.c.obj"
+  "CMakeFiles/coursework_5_tx.dir/C_/Freescale/KSDK_1.3.0/TEST/platform/utilities/src/fsl_misc_utilities.c.obj"
+  "CMakeFiles/coursework_5_tx.dir/C_/Freescale/KSDK_1.3.0/TEST/platform/utilities/src/print_scan.c.obj"
+  "CMakeFiles/coursework_5_tx.dir/C_/Freescale/KSDK_1.3.0/TEST/software/lpuart_non_blocking_Tx_CW5/SEGGER_RTT.c.obj"
+  "CMakeFiles/coursework_5_tx.dir/C_/Freescale/KSDK_1.3.0/TEST/software/lpuart_non_blocking_Tx_CW5/SEGGER_RTT_printf.c.obj"
+  "CMakeFiles/coursework_5_tx.dir/C_/Freescale/KSDK_1.3.0/TEST/software/lpuart_non_blocking_Tx_CW5/board.c.obj"
+  "CMakeFiles/coursework_5_tx.dir/C_/Freescale/KSDK_1.3.0/TEST/software/lpuart_non_blocking_Tx_CW5/coursework_tx_main.c.obj"
+  "CMakeFiles/coursework_5_tx.dir/C_/Freescale/KSDK_1.3.0/TEST/software/lpuart_non_blocking_Tx_CW5/fsl_lpuart_irq.c.obj"
+  "CMakeFiles/coursework_5_tx.dir/C_/Freescale/KSDK_1.3.0/TEST/software/lpuart_non_blocking_Tx_CW5/gpio_pins.c.obj"
+  "CMakeFiles/coursework_5_tx.dir/C_/Freescale/KSDK_1.3.0/TEST/software/lpuart_non_blocking_Tx_CW5/hardware_init.c.obj"
+  "CMakeFiles/coursework_5_tx.dir/C_/Freescale/KSDK_1.3.0/TEST/software/lpuart_non_blocking_Tx_CW5/pin_mux.c.obj"
+  "release/coursework_5_tx.elf"
+  "release/coursework_5_tx.elf.manifest"
+  "release/coursework_5_tx.elf.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang ASM C)
+  include(CMakeFiles/coursework_5_tx.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
